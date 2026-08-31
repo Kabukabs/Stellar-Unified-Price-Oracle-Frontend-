ee46c17078c1f8f8d28814512350ef6ef7e39983
soyaya <davidlovedavid1015@gmail.com>
2026-08-31 12:05:28 +0100

    feat(webhooks): add developer subscription management UI (#453)

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { PerformanceOverlay } from './components/PerformanceOverlay'
 import { ApiVersionBanner } from './components/ApiVersionBanner'
 import { InstallPrompt } from './components/InstallPrompt'
 import { PwaUpdateBanner } from './components/PwaUpdateBanner'
+import { Webhooks } from './pages/Webhooks'
 import {
   LazyApiDocs,
   LazyDashboard,
@@ -99,6 +100,7 @@ export function AppContent(): ReactElement {
                 </RouteSuspense>
               }
             />
+            <Route path="/webhooks" element={<Webhooks />} />
             <Route
               path="*"
               element={
